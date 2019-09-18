f4d18948b9fde6f14cefe4a878b7b1856f5e3f68
lius-g <55494978+lius-g@users.noreply.github.com>
2019-09-18 22:39:55 +0800

    Update ocserv.sh

--- ocserv.sh
+++ ocserv.sh
@@ -72,7 +72,7 @@ Download_ocserv(){
 	mkdir "ocserv" && cd "ocserv"
 	wget "ftp://ftp.infradead.org/pub/ocserv/ocserv-${ocserv_ver}.tar.xz"
 	[[ ! -s "ocserv-${ocserv_ver}.tar.xz" ]] && echo -e "${Error} ocserv 源码文件下载失败 !" && rm -rf "ocserv/" && rm -rf "ocserv-${ocserv_ver}.tar.xz" && exit 1
-	tar -xJf ${ocserv_ver}.tar.xz && cd ocserv-${ocserv_ver}
+	tar -xJf 0.12.4.tar.xz && cd ocserv-0.12.4
 	./configure
 	make
 	make install
